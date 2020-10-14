7651b9b083dd81bf2885af0deda013fdd4d6d6e7
Omair Muhi <omairmuhi@icloud.com>
2020-10-14 08:32:09 -0400

    Subject: Update README, add screenshot, minor JS changes Problem: NA Solution: NA Note: NA

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ A web-application that allows the user to select specific criteria and then gene
 ## Usage 
 
 Access web-page here: (https://omair-muhi.github.io/password-generator/)
+![Password-generator main screen!](images/password-generator-screenshot.png)
 
 ## Credits
 * UoT FSF Bcamp - this template
@@ -22,14 +23,19 @@ Access web-page here: (https://omair-muhi.github.io/password-generator/)
 MIT License
 
 ## Tests
+#### Main
 1. Test with 8 characters with all characters included.
 2. Test with 128 characters with all characters included.
 3. Test with 64 characters with lower-case and upper-case characters only.
 4. Test with 64 characters with numeric and special characters only.
-5. Test with 8-128 character length with all characters unselected.
-6. Test with 32 characters with only lower-case characters selected.
-7. Test with 32 characters with only upper-case characters selected.
-8. Test with 32 characters with only numeric characters selected.
-9. Test with 32 characters with only special characters selected.
+#### Edge Cases
+1. Generate 8 character password with only lower-case characters
+2. Generate 16 character password with only upper-case characters
+3. Generate 32 character password with only numeric characters
+4. Generate 64 character password with only special characters
+5. Generate 7 character password
+6. Generate 129 character password
+#### Corner Case
+1. Generate password with no criteria selected 
 ---
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowerCaseChars =
-  ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-  'n','o','p','q','r','s','t','u','v','w','x','y','z']; // static list of lower-case characters
-var upperCaseChars = 
-['A','B','C','D','E','F','G','H','I','J','K','L','M',
-'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; // static list of upper-case characters
-var numberChars = 
-['0','1','2','3','4','5','6','7','8','9']; // static list of numeric characters
-var specialChars = 
-[" ","!","\"","#","$","%","&","'","(",")","*",
-"+",",","-",".","/",":",";","<","=",">","?",
-"@","[","\\","]","^","_","`","{","|","}","~"]; // 33 Special characters based on OWASP list
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // static list of lower-case characters
+var upperCaseChars =
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // static list of upper-case characters
+var numberChars =
+  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; // static list of numeric characters
+var specialChars =
+  [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*",
+    "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?",
+    "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]; // 33 Special characters based on OWASP list
 
 /*
 * FUNCTION: generatePassword()
@@ -20,29 +20,29 @@ var specialChars =
 * RETURNS: password - a string meeting user criteria for password
 */
 function generatePassword(passwordCriteria) {
-   // This is an empty string which will 
-   // grow as more characters are added to it
+  // This is an empty string which will 
+  // grow as more characters are added to it
   var password = "";
   var passwordChars = []; // a 2D array
   // Following IF-checks will incrementally
   // build a global pool of characters to choose from based
   // on the passwordCriteria selected by the user
-  if(passwordCriteria.lower === true) {
+  if (passwordCriteria.lower === true) {
     // add lower-case chars if user wants
     // to include it in their password 
     passwordChars.push(lowerCaseChars);
   }
-  if(passwordCriteria.upper === true) {
+  if (passwordCriteria.upper === true) {
     // add upper-case chars if user wants
     // to include it in their password 
     passwordChars.push(upperCaseChars);
   }
-  if(passwordCriteria.numeric === true) {
+  if (passwordCriteria.numeric === true) {
     // add numeric chars if user wants
     // to include it in their password 
     passwordChars.push(numberChars);
   }
-  if(passwordCriteria.special === true) {
+  if (passwordCriteria.special === true) {
     // add special chars if user wants
     // to include it in their password 
     passwordChars.push(specialChars);
@@ -55,12 +55,12 @@ function generatePassword(passwordCriteria) {
   }
   // Use a for-loop to fill characters matching requested length
   // position-by-position
-  for (var char=0; char<passwordCriteria.length; char++) {
+  for (var char = 0; char < passwordCriteria.length; char++) {
     // For each character position in password string:
     // Step 1: Choose from upto 4 pools by generating a random # between 0-3(max)
-    var globalPoolIndex = Math.floor(Math.random()*passwordChars.length);
+    var globalPoolIndex = Math.floor(Math.random() * passwordChars.length);
     // Step 2: Choose a random index from the chosen pool
-    var charPoolIndex = Math.floor(Math.random()*passwordChars[globalPoolIndex].length);
+    var charPoolIndex = Math.floor(Math.random() * passwordChars[globalPoolIndex].length);
     // Step 3: Index into 2D array and extract a password char 
     // Step 3.1: Concatenate chosen character to password string
     // console.log("char="+char+": globalPoolIndex="+globalPoolIndex+" charPoolIndex="+charPoolIndex); // enable for debugging
@@ -76,7 +76,7 @@ function writePassword() {
   // 1. Prompt for length of password
   do {
     var passwordLength = prompt("Choose a password length (8-128 chracters)");
-  } while(passwordLength < 8 || passwordLength > 128)
+  } while (passwordLength < 8 || passwordLength > 128)
   passwordCriteria.length = passwordLength;
   // 2. Prompt for character types
   passwordCriteria.lower = confirm("Include lower-case characters?"); // lower-case
